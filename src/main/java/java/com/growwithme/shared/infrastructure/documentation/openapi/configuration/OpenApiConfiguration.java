dc8d6e9445dc7aeb7b332b1179929caf3283c901
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-04-10 00:32:12 -0500

    feat: add shared bounded context with common configuration for open api, naming strategy and auditable models.

---
+++ src/main/java/java/com/growwithme/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -0,0 +1,43 @@
+package java.com.growwithme.shared.infrastructure.documentation.openapi.configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfiguration {
+    @Bean
+    public OpenAPI learningPlatformOpenApi() {
+        // General configuration
+        var openApi = new OpenAPI();
+        openApi
+                .info(new Info()
+                        .title("Grow With Me API")
+                        .description("Grow With Me - CupOfTech REST API documentation.")
+                        .version("v1.0.0")
+                        .license(new License().name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Grow With Me API Backend Documentation")
+                        .url("https://github.com/upc-pre-SI729-2402-WX51-G2-CupOfTech/GrowWithMe-Backend"));
+        // Add Security Scheme
+        final String securitySchemeName = "bearerAuth";
+        openApi.addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+        // Return OpenAPI configuration object
+        return openApi;
+    }
+}
