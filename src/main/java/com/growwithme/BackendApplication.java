5971cf2899335f397f3932145436369783de39e0
Favio Sebastián Landeo Simeón <faviomsg05@gmail.com>
2025-06-18 13:38:06 -0500

    fix: fix enable jpa auditing for the backend.

--- src/main/java/com/growwithme/BackendApplication.java
+++ src/main/java/com/growwithme/BackendApplication.java
@@ -2,7 +2,9 @@ package com.growwithme;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
